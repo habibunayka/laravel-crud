9314ad6cf8edfdb53e4d6f4eb066dae3b1f213fb
unknown <habibunayka@gmail.com>
2024-03-14 11:20:43 +0700

    deskripsi commit

--- routes/web.php
+++ routes/web.php
@@ -14,19 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::controller('PostController')->group(function() {
-//     Route::get('/', [PostController::class, 'index'])->name('posts.index');
-//     Route::get('/creatos', [PostController::class, 'create'])->name('posts.create');
-//     Route::get('/edit/{posts}', [PostController::class, 'edit'])->name('posts.edit');
-//     Route::post('/store', [PostController::class, 'store'])->name('posts.store');
-//     Route::post('/update/{post}', [PostController::class, 'update'])->name('posts.update');
-//     Route::get('/delete/{post}', [PostController::class, 'delete'])->name('posts.delete');
-// });
-Route::controller(PostController::class)->group(function() {
-    Route::get('/','index');
-    Route::get('/edit/{posts}','edit');
-    Route::get('/create','create');
-    Route::post('/update/{post}','update');
-    Route::post('/store','store');
-    Route::get('/delete/{post}','delete');
+Route::controller('PostController')->group(function() {
+    Route::get('/', [PostController::class, 'index'])->name('posts.index');
+    Route::get('/creatos', [PostController::class, 'create'])->name('posts.create');
+    Route::get('/edit/{posts}', [PostController::class, 'edit'])->name('posts.edit');
+    Route::post('/store', [PostController::class, 'store'])->name('posts.store');
+    Route::post('/update/{post}', [PostController::class, 'update'])->name('posts.update');
+    Route::get('/delete/{post}', [PostController::class, 'delete'])->name('posts.delete');
 });
+// Route::controller(PostController::class)->group(function() {
+//     Route::get('/','index');
+//     Route::get('/edit/{posts}','edit');
+//     Route::get('/create','create');
+//     Route::post('/update/{post}','update');
+//     Route::post('/store','store');
+//     Route::get('/delete/{post}','delete');
+// });
